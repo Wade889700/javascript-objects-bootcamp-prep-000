50c0ffd228fb798b738ca2e0f92f94290356d916
severus889700 <severus889700@gmail.com>
2018-10-02 04:52:07 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,10 +1,11 @@
 function updatePlaylist(playlist, name_of_artist, title_of_song){
-  return playlist[name_of_artist]=title_of_song}
+  playlist.name_of_artist='title_of_song';
+  return playlist
 }
 
 function removeFromPlaylist(playlist, name_of_artist){
  delete playlist.name_of_artist;
- return playlist[name_of_artist]=title_of_song}
+ return playlist
 }
 
 var playlist = new Object({artist_names:"song titles"});
